b77f8b44c152d3b19816c61c8c46ba005be0af5b
vncloudsco <support@vnclouds.edu.vn>
2018-12-10 17:56:23 +0700

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-curl https://rclone.org/install.sh | sudo bash
-wget -O - https://backup.vnclouds.co/install.sh > /root/.config/backup_vnc.sh --no-check-certificate
-touch /etc/cron.d/backup
-cat <<EOT >> /etc/cron.d/backup
-echo "0 1 * * * root /root/.config/backup_vnc.sh > /dev/null 2>&1 /etc/cron.d" >> /etc/cron.d/backup
-EOT
+echo "noi dung ma hoa";
